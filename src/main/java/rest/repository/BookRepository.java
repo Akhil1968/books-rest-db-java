c729466245d252ab5178bda6d4479c3c182cc67e
Akhilanand Singh <akhilanand.singh@IN-IT1605.local>
2019-08-03 04:01:04 +0530

    first commit

---
+++ src/main/java/rest/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package rest.repository;
+
+
+import rest.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
